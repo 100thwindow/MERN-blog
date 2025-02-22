325fdf554bbae5a593a643f15467d1eab036035b
yigit <114931512+100thwindow@users.noreply.github.com>
2025-02-22 17:24:28 +0300

    tweak

--- server/index.js
+++ server/index.js
@@ -11,7 +11,9 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://mern-blog-gray-pi.vercel.app", credentials: true })
+);
 
 // Connect to MongoDB
 mongoose
