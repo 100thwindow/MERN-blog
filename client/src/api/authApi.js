e13eca5f93fa296379c9261cf6ede65ee58819c1
yigit <114931512+100thwindow@users.noreply.github.com>
2025-03-01 16:04:57 +0300

    tweak

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth"; // Adjust if your backend runs elsewhere
+
+const API_URL = `${import.meta.env.VITE_API_URL}/api/auth`; // Backend URL
 
 export const registerUser = async (userData) => {
   try {
